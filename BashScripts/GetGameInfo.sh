7e443392acbfe0a7b0332b2d542c211066ccc6b3
meso_test <45484407+HellInsider@users.noreply.github.com>
2022-09-24 01:02:54 +0300

    Added comments

--- BashScripts/GetGameInfo.sh
+++ BashScripts/GetGameInfo.sh
@@ -2,3 +2,5 @@ id=$1
 url="http://store.steampowered.com/api/appdetails?appids=${id}" 
 output="GamesInfo/GameInfoId_$id.txt"
 curl -L "${url}" -o "${output}"
+
+#takes game id as cmd argument and returns file of game to GamesInfo
