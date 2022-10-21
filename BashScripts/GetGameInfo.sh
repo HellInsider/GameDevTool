5d85e0bd8a445a6558e87959a697c30fa9d1c387
meso_test <lfyz20012@mail.ru>
2022-10-22 01:39:19 +0300

    BD Backup and scripts upload

--- BashScripts/GetGameInfo.sh
+++ BashScripts/GetGameInfo.sh
@@ -1,6 +1,9 @@
+
 id=$1
 url="http://store.steampowered.com/api/appdetails?appids=${id}" 
-output="GamesInfo/GameInfoId_$id.txt"
+output="C:/Dev/Projects/GameDevTool/BashScripts/GamesInfo/GameInfoId_$id.txt"
+#output="GamesInfo/GameInfoId_$id.txt"
 curl -L "${url}" -o "${output}"
+
 
 #takes game id as cmd argument and returns file of game to GamesInfo
